cfe46e265e1cf4b2574e7670ab6e64b69ceb0d33
rhsProgrammingClub <89669258+rhsProgrammingClub@users.noreply.github.com>
2021-08-27 16:39:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var mini = true;
+var print = console.log;
+
+function toggleSidebar() {
+	if (mini) {
+		document.getElementById("mySidebar").style.width = "320px";
+
+		this.mini = false;
+	} else {
+		document.getElementById("mySidebar").style.width = "100px";
+
+		this.mini = true;
+	}
+}
+
+cardlist = document.querySelectorAll(".Hansoncard");
+
+
+
+
+cardlist.forEach(function (card) {
+	var bgcolor = getComputedStyle(card).getPropertyValue('--card-color');
+	var isenter = false;
+	card.addEventListener('mousemove', function (e) {
+		if(isenter){
+			card.style.transition = "transform 0.1s";
+			let xpos = e.pageX - card.offsetLeft;
+			let ypos = e.pageY - card.offsetTop;
+			let xy = xpos + " " + ypos;
+			let ratio = card.offsetWidth/15*card.offsetHeight/card.offsetWidth
+			let rotx = (card.offsetWidth / 2 - xpos) / ratio;
+			let roty = (card.offsetHeight / 2 - ypos) / ratio;
+			
+			let bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", 200, from(rgba(255,255,255,0.2)), to(rgba(255,255,255,0.0))), " + bgcolor;
+
+			card.style.transform = "scale3D(1.05,1.05,1.05) rotateY(" + rotx + "deg) rotateX(" + roty + "deg) translateZ(20px)";
+			card.style.background = bgWebKit;
+		}
+
+	});	
+	card.addEventListener("mouseenter", function(e){
+		isenter = true;
+		card.style.transition = "transform 0.1s";
+		let xpos = e.pageX - card.offsetLeft;
+		let ypos = e.pageY - card.offsetTop;
+		let ratio = card.offsetWidth/15*card.offsetHeight/card.offsetWidth
+		let rotx = (card.offsetWidth / 2 - xpos) / ratio;
+		let roty = (card.offsetHeight / 2 - ypos) / ratio;
+		
+		card.style.transform = "scale3D(1.05,1.05,1.05) rotateY(" + rotx + "deg) rotateX(" + roty + "deg) translateZ(20px)";
+	});
+	card.addEventListener('mouseleave', function (e) {
+		isenter = false;
+	card.style.background = bgcolor;
+		card.style.transition = "transform 0.5s"
+		card.style.transform = "none";
+	});
+
+
+
+});
+

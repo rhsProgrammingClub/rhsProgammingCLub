89395ed2231508bfd414b2861870c9759806da3b
Hanson-Sun <65196939+Hanson-Sun@users.noreply.github.com>
2022-06-17 17:11:00 -0700

    fixed mobile layout

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var mini = true;
-var print = console.log;
-
-function toggleSidebar() {
-	if (mini) {
-		document.getElementById("mySidebar").style.width = "320px";
-
-		this.mini = false;
-	} else {
-		document.getElementById("mySidebar").style.width = "100px";
-
-		this.mini = true;
-	}
-}
-
 cardlist = document.querySelectorAll(".Hansoncard");
 
 
